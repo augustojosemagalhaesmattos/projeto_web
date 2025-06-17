f3d27495befc1f94e30b763b633e275006f094fa
augustojosemagalhaesmattos <augustommjf@gmail.com>
2025-06-17 19:54:37 -0300

    atividade 03

--- atividade_3_arrays_filtros/js/script.js
+++ atividade_3_arrays_filtros/js/script.js
@@ -1,23 +1,85 @@
-const produtos = [
-    {nome: "Sabonete Natural", preço: 15.00, categoria: "cosmetico" },
-    {nome: "Shampoo Organico", preço: 25.00, categoria: "cosmetico" },
-    {nome: 'Granola Artesanal', preço: 18.50, categoria: "alimento" },
-    {nome: 'Mel Puro', preço: 22.00, categoria: "alimento"},
-];
+document.addEventListener('DOMContentLoaded', function(){
+    const saida = document.getElementById('saida');
+    const total = document.getElementById('resultado');
+    const limpar = elemento => elemento.innerHTML = '';
 
-document.getElementById('todos').addEventListener('click'), function() {
-     const todos = produtos.filter(function(produtos) {
-        return produtos
-     })
-    
-    
-    
-    
-    // Exibindo o resultado na página
-    document.getElementById('resultado-paraCada').innerHTML = `
-        <strong>Array original:</strong> ${JSON.stringify(numeros)}<br>
-        <strong>Iteração com forEach:</strong><br>
-        ${resultado}
-        <small>Observação: forEach não retorna um novo array</small>
-    `;
-}
+    const produtos = [
+        { nome: 'Sabonete natural', preco: 15.00, categoria: 'Cosméticos'},
+        { nome: 'Shampoo orgânico', preco: 25.00, categoria: 'Cosméticos'},
+        { nome: 'Granola artezanal', preco: 18.50, categoria: 'Alimentos'},
+        { nome: 'Mel puro', preco: 22.00, categoria: 'Alimentos'},
+    ];
+
+    document.getElementById('opcoes').addEventListener('change', function(){
+        
+        if (opcoes.value === 'al') {
+            limpar(saida);
+
+            let calcular = 0
+
+            let produtoFiltrado = produtos.filter(function (valores) {
+                return valores.categoria === 'Alimentos'; 
+            });
+
+            produtoFiltrado.forEach(function (valores) {
+
+                const elemento = document.createElement('li');
+
+                elemento.innerHTML = `<strong>${valores.nome}</strong><br>
+                R$: ${valores.preco}`;
+
+                saida.appendChild(elemento);
+
+                calcular = calcular + valores.preco
+            });
+            
+            total.innerHTML = `Total: ${calcular}`;
+
+        }
+
+        else if (opcoes.value === 'cos') {
+            limpar(saida);
+
+            let calcular = 0
+
+            let produtoFiltrado = produtos.filter(function (valores) {
+                return valores.categoria === 'Cosméticos'; 
+            });
+
+            produtoFiltrado.forEach(function (valores) {
+
+                const elemento = document.createElement('li');
+
+                elemento.innerHTML = `<strong>${valores.nome}</strong><br>
+                R$: ${valores.preco}`;
+
+                saida.appendChild(elemento);
+
+                calcular = calcular + valores.preco
+            });
+            
+            total.innerHTML = `Total: ${calcular}`;
+
+        }
+
+        else {
+            limpar(saida);
+
+            let calcular = 0
+
+            produtos.forEach(function (valores) {
+
+                const elemento = document.createElement('li');
+
+                elemento.innerHTML = `<strong>${valores.nome}</strong><br>
+                R$: ${valores.preco}`;
+
+                saida.appendChild(elemento);
+
+                calcular = calcular + valores.preco
+            });
+            
+            total.innerHTML = `Total: ${calcular}`;
+        }
+    });
+});
